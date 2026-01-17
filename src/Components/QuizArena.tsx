c4f506285cdb754155ffb9b9bf3abcdc93c476ec
Miguel Louwagie <miguellouwagie@gmail.com>
2026-01-17 15:42:07 +0100

    fix(types): fix strict null check in shuffleArray for QuizArena

--- src/Components/QuizArena.tsx
+++ src/Components/QuizArena.tsx
@@ -10,7 +10,7 @@ const shuffleArray = <T,>(array: T[]): T[] => {
     const newArr = [...array];
     for (let i = newArr.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
+        const temp = newArr[i]!; newArr[i] = newArr[j]!; newArr[j] = temp;
     }
     return newArr;
 };
